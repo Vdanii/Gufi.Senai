5ea967253c999422e4121abd4ac050cb2fe02102
Vdanielly53@gmail.com <FIC@info.local>
2020-03-12 17:15:59 -0300

    12/03/2020

---
+++ Senai.Gufi.WebApi/Controllers/TiposUsuarioController.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Senai.Gufi.WebApi.Domains;
+using Senai.Gufi.WebApi.Interfaces;
+using Senai.Gufi.WebApi.Repositories;
+
+// For more information on enabling MVC for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace Senai.Gufi.WebApi.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/[controller]")]
+    [ApiController]
+
+    public class TiposUsuarioController : ControllerBase
+    {
+        private ITipoUsuarioRepository _tipoUsuarioRepository;
+
+        public TiposUsuarioController()
+        {
+            _tipoUsuarioRepository = new TipoUsuarioRepository();
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok(_tipoUsuarioRepository.Listar());
+        }
+
+        [HttpGet("{id}")]
+        public IActionResult GetById(int id)
+        {
+            return StatusCode(200, _tipoUsuarioRepository.BuscarPorId(id));
+        }
+
+        [HttpPost]
+        public IActionResult Post(TipoUsuario novoTipoUsuario)
+        {
+            _tipoUsuarioRepository.Cadastrar(novoTipoUsuario);
+
+            return StatusCode(201);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult Put(int id, TipoUsuario tipoUsuarioAtualizado)
+        {
+            TipoUsuario tipoUsuarioBuscado = _tipoUsuarioRepository.BuscarPorId(id);
+
+            if (tipoUsuarioBuscado != null)
+            {
+                try
+                {
+                    _tipoUsuarioRepository.Atualizar(id, tipoUsuarioAtualizado);
+
+                    return StatusCode(200);
+                }
+                catch (Exception erro)
+                {
+                    return BadRequest(erro);
+                }
+            }
+
+            return StatusCode(404);
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult Delete(int id)
+        {
+            TipoUsuario tipoUsuarioBuscado = _tipoUsuarioRepository.BuscarPorId(id);
+
+            if (tipoUsuarioBuscado == null)
+            {
+                return NotFound();
+            }
+
+            _tipoUsuarioRepository.Deletar(id);
+
+            return StatusCode(202);
+        }
+    }
+}
